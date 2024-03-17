e1b7c551c7a1bb740a1b490048cbf763a4f54a61
sodkiewiczm <sodkiewiczm@gmail.com>
2024-03-17 13:52:04 +0100

    Setup tests

---
+++ perfTest/run.sh
@@ -0,0 +1,2 @@
+#K6_WEB_DASHBOARD=true K6_WEB_DASHBOARD_EXPORT=nootel-html-report.html k6 run ./nootel-script.js
+K6_WEB_DASHBOARD=true K6_WEB_DASHBOARD_EXPORT=otel-html-report.html k6 run ./otel-script.js
